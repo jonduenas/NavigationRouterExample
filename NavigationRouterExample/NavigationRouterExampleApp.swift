6c95b482abe2e6843e4e94b9b47ced2129aa14e1
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2024-05-25 16:22:15 -0400

    Initial Commit

---
+++ NavigationRouterExample/NavigationRouterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationRouterExampleApp.swift
+//  NavigationRouterExample
+//
+//  Created by Jon Duenas on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationRouterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
